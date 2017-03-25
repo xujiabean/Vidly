e90bc3e725d936d0315d8d53771ef098db7c6379
xujiabean <xujia_catherine@sina.com>
2017-03-25 19:36:46 -0400

    Initial Checkin
    Initial Checkin

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Dump(int? id)
+        {
+            if(id.HasValue)
+                return Content(String.Format("Dump Data with ID: {0}", id));
+            else
+                return Content(String.Format("Dump Empty Data!"));
+        }
+    }
+}
---
+++ Vidly/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+        //public MembershipType MembershipType { get; set; }
+        public byte MembershipType { get; set; }
+
+        public DateTime? Birthday { get; set; }
+    }
+}
